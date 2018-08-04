815f7d4054c77d8a9725cf919d96d5caf131c6cc
Aaron <AaronWolfenden@hotmail.co.uk>
2018-08-04 07:26:04 +0100

    Moved game functions to game_functions.py

--- SpaceInvaders/space_invaders.py
+++ SpaceInvaders/space_invaders.py
@@ -1,9 +1,12 @@
 import sys
 import pygame
+
 from SpaceInvaders.settings import Settings
 from SpaceInvaders.ship import Ship
+import SpaceInvaders.game_functions as gf
 
 def run_game():
+    """ Initialises game """
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
@@ -14,9 +17,6 @@ def run_game():
 
     while True:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
 
         screen.fill(ai_settings.bg_colour)
         ship.blitme()
