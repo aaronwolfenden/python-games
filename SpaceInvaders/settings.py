9ff5e36c303a50591e6917c13602d3e57e10600c
Aaron <AaronWolfenden@hotmail.co.uk>
2018-08-04 06:35:18 +0100

    Initial commit

---
+++ SpaceInvaders/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """ A Class Which Stores All Game Related Settings """
+
+    def __init__(self):
+        """ Initialises All Game Settings """
+        self.screen_width = 500 #1920
+        self.screen_height = 500 #1080
+        self.bg_colour = 230, 230, 230
---
+++ SpaceInvaders/space_invaders.py
@@ -0,0 +1,27 @@
+import sys
+import pygame
+from SpaceInvaders.settings import Settings
+from SpaceInvaders.ship import Ship
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Space Invaders")
+
+    ship = Ship(screen)
+
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(ai_settings.bg_colour)
+        ship.blitme()
+
+        pygame.display.flip()
+
+
+run_game()
