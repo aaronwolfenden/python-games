162a718b289170c9c6e1a8945f9aa71807b330df
Aaron <AaronWolfenden@hotmail.co.uk>
2018-08-04 16:02:22 +0100

    Added bullet.py Added bullet specific settings Fixed ship controls Added bullet deleting

--- SpaceInvaders/game_functions.py
+++ SpaceInvaders/game_functions.py
@@ -2,19 +2,21 @@ import sys
 
 import pygame
 
+from SpaceInvaders.bullet import Bullet
 
-def check_events(ship):
+
+def check_events(si_settings, screen, ship, bullets):
     """ Responses to keypress and mouse events """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, si_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, si_settings, screen, ship, bullets):
     """ Respond to key presses """
     if event.key == pygame.K_RIGHT:
         # Move the ship to the right
@@ -22,6 +24,10 @@ def check_keydown_events(event, ship):
     elif event.key == pygame.K_LEFT:
         # Move the ship to the left
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group
+        new_bullet = Bullet(si_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -29,13 +35,19 @@ def check_keyup_events(event, ship):
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_RIGHT:
             ship.moving_right = False
-    elif event.key == pygame.K_LEFT:
-        ship.moving_left = False
+        elif event.key == pygame.K_LEFT:
+            ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship):
+
+def update_screen(ai_settings, screen, ship, bullets):
     """ Update images on the screen and flip to the new screen. """
     # Redraws the screen during each pass through the loop
     screen.fill(ai_settings.bg_colour)
+
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
 
     # Makes the most recently drawn screen visible
